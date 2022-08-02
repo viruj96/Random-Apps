452d121a58485ae5d7913cfdca9abb31165d6ee4
Viruj <virujbala.96@gmail.com>
2022-08-02 23:39:58 +0400

    delete and parse functions

--- pearson_regression/js/inputs.js
+++ pearson_regression/js/inputs.js
@@ -1,29 +1,53 @@
 function addRow() {
-	const table = $('tbody');
-	const nRows = table.rows.length;
-	const tr = table.insertRow(nRows);
+	const tbody = $('#data-table tbody');
+	const nRows = tbody.rows.length;
+	let rowNumber = 1;
+	if (nRows > 0) {
+		const rows = tbody.children;
+		const lastRow = rows.item(rows.length - 1);
+		rowNumber = parseInt(lastRow.id) + 1;
+	}
+	const tr = tbody.insertRow(nRows);
 	const thCount = $$('th').length;
 	const attr = [$('input[name=x-data]:checked').value, $('input[name=y-data]:checked').value];
-	const bg = nRows % 2 !== 0 ? 'lightblue' : 'lightgrey';
 	for (let i = 0; i < thCount; i++) {
 		const td = tr.insertCell(i);
 		if (i === 0) {
-			td.innerHTML = i + 1 + nRows;
+			tr.id = rowNumber;
+			td.innerHTML = rowNumber; // Set row number
 		} else {
-			const input = makeTag('input');
-			input.style.backgroundColor = bg;
+			const input = create('input');
+			css(input, { 'background': 'transparent' });
 			input.setAttribute('type', attr[i - 1]);
 			if (attr[i - 1] === 'number')
 				input.setAttribute('step', '0.01');
 			td.appendChild(input);
 		}
 	}
+	on(tr, 'click', handleRowClick)
+}
+
+function deleteRow() {
+	$$('.highlighted').forEach(row => {
+		row.parentNode.removeChild(row);
+	});
 }
 
 function importData() {
 	const path = $('#file-path').value;
 	if (!path) {
 		$('input[type=file]').click();
+	}
+}
+
+function parseData() {
+	const textarea = $('#textarea');
+	const rows = $('#data-table tbody tr');
+	if (!rows || rows.length === 0) {
+		const p = create('p');
+		p.innerHTML = 'Add data before processing';
+		css(p, { 'color': 'red', 'margin': 0, 'margin-left': '2%' });
+		textarea.appendChild(p);
 	}
 }
 
