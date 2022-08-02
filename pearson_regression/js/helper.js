372291dab363779c6e73789b84eabfbe8e8575fb
Viruj <virujbala.96@gmail.com>
2022-08-02 23:38:56 +0400

    helper functions

--- pearson_regression/js/helper.js
+++ pearson_regression/js/helper.js
@@ -1,5 +1,18 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
-const makeTag = (tag) => document.createElement(tag);
+const create = (tag) => document.createElement(tag);
+const toggle = (selector, attr, bool) => $(selector)[attr] = bool;
+const addClass = (element, className) => element.classList.add(className);
+const removeClass = (element, className) => element.classList.remove(className);
+const hasClass = (element, className) => element.classList.contains(className);
+const css = (element, styleObject) => {
+	let cssText = '';
+	Object.keys(styleObject).forEach(key => cssText += `${key}: ${styleObject[key]}; `);
+	element.style.cssText = cssText;
+};
 
-const print = (...args) =>  console.log(args.reduce((acc, cur) => `${acc} ${cur}`));
+const on = (element, event, callback) => element.addEventListener(event, callback);
+
+const print = (...args) => console.log(args.reduce((acc, cur) => `${acc} ${cur}`));
+
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
